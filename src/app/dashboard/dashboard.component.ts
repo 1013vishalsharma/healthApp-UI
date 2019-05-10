7b46f383efa9742f18afb7565e5450f63c1515af
vishal <1013vishalsharma@gmail.com>
2019-05-10 09:40:06 +0530

    add activity connected to node js

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { DashboardService } from '../dashboard/dashboard.service';
+import { AddActivity } from '../dashboard/AddActivity';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,19 +10,34 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
   addActivityForm: FormGroup;
-  constructor() { }
+  workoutTypes = ['Legs', 'Chest', 'Biceps', 'Triceps', 'Back', 'Abs', 'Shoulders', 'Cardio'];
+  foodTypes = ['Healthy', 'Unhealthy'];
+  addActivityData: AddActivity;
+  constructor(private dashboadService: DashboardService) { }
 
   ngOnInit() {
     this.addActivityForm = new FormGroup({
       workoutDate: new FormControl(),
-      workoutTime: new FormControl(),
+      workoutDuration: new FormControl(),
       workoutType: new FormControl(),
       foodType: new FormControl()
     })
   }
 
   addActivity(): void{
-    console.log(this.addActivityForm.value);
+    console.log(this.addActivityForm.value)
+    //  this.addActivityData = new AddActivity({
+    //    this.addActivityForm.get('workoutDate').value,
+    //    this.addActivityForm.get('workoutType').value,
+    //    this.addActivityForm.get('workoutDate').value,
+    //    this.addActivityForm.get('workoutDate').value,
+    //  });
+
+    this.dashboadService.addActivityToMongo(this.addActivityForm.value)
+    .subscribe((data: {}) => {
+      console.log(data);
+    });
+    
   }
 
 }
